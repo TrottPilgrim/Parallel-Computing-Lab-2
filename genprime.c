92ea46dbb1b2f3a505934fde67b5f720c32e2821
Thomas Lim <tl1860@nyu.edu>
2019-04-13 19:46:03 -0400

    testing

--- genprime.c
+++ genprime.c
@@ -15,8 +15,21 @@
  *  c: the interval from the previous prime (curr - prev)
  */
 
+typedef enum {false, true} bool;
+
+/******** Globals ********/
+int N; /* a positive number */
+int thread_count; 
+bool* isPrime;
+int* primeVals;
+int primeValSize;
+int floor;
+
+/* Function declarations */
+void parallel();
+
 int main(int argc, char *argv[]){
-    
+
     char* output[100] = "";
     FILE * fp;
 
@@ -27,16 +40,55 @@ int main(int argc, char *argv[]){
     }
     //argv[1] is N
     //argv[2] is t
+    N = strtol(argv[1], NULL, 10);
+    thread_count = strtol(argv[2], NULL, 10);
 
-    sprintf(output, "%d.txt", argv[1]);
-    fp = fopen(output, "w");
-    if (!fp)
+    isPrime = (bool *) malloc(N * sizeOf(bool));
+    if (!isPrime)
     {
-        printf("Cannot create the file %s\n", output);
+        printf("Can't allocate prime boolean array.\n");
         exit(1);
     }
 
-    fclose(fp);
-    exit(0);
+    for (int i = 0; i < N; i++)
+        isPrime[i] = true;
+    
+    int thread_rank = omp_get_thread_num();
+    if (thread_rank == 0)
+    {
+        sprintf(output, "%d.txt", N);
+        fp = fopen(output, "w");
+        if (!fp)
+        {
+            printf("Cannot create the file %s\n", output);
+            exit(1);
+        }
+        for (int i = 2; i < primeValSize; i++)
+            fprintf(fp, "%d %d %d\n", i, primeValues[i]);
+        fclose(fp);
+        exit(0);
+    }
 
+}
+
+void parallel() 
+{
+    int current;
+    floor = (N + 1) / 2 + 1;
+    for (current = 2; current < floor; current++)
+    {
+        if (isPrime[current])
+        {
+            # pragma omp parallel num_threads(thread_count)
+            {
+                int i;
+                # pragma omp parallel for
+                for (i = current; i < N; i += current)
+                    isPrime[i] = false;
+            }
+
+            primeVals[primeValuesSize] = current;
+            primeValSize++;
+        }
+    }
 }
