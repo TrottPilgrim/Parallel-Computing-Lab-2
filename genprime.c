706bcdff1a573a76daf7e986fcd713988976f61b
Thomas Lim <tl1860@nyu.edu>
2019-04-13 20:48:18 -0400

    runs with correct results

--- genprime.c
+++ genprime.c
@@ -22,7 +22,7 @@ int N; /* a positive number */
 int thread_count; 
 bool* isPrime;
 int* primeVals;
-int primeValSize;
+int primeValSize = 0;
 int lastNum;
 
 /* Function declarations */
@@ -30,6 +30,7 @@ void parallel();
 
 int main(int argc, char *argv[]){
 
+    double tstart = 0.0, ttaken;
     char output[100] = "";
     FILE * fp;
 
@@ -52,12 +53,14 @@ int main(int argc, char *argv[]){
 
     for (int i = 0; i < N; i++)
         isPrime[i] = true;
-    
+    tstart = omp_get_wtime();
     parallel();
+    ttaken = omp_get_wtime() - tstart;
 
     int thread_rank = omp_get_thread_num();
     if (thread_rank == 0)
     {
+        printf("Time taken for the main part: %f\n", ttaken);
         sprintf(output, "%d.txt", N);
         fp = fopen(output, "w");
         if (!fp)
@@ -65,8 +68,9 @@ int main(int argc, char *argv[]){
             printf("Cannot create the file %s\n", output);
             exit(1);
         }
-        for (int i = 2; i < primeValSize; i++)
-            fprintf(fp, "%d %d\n", i, primeVals[i]);
+        fprintf(fp, "%d, %d, %d\n", 1, primeVals[0], 0);
+        for (int i = 1; i < primeValSize; i++)
+            fprintf(fp, "%d, %d, %d\n", i + 1, primeVals[i], primeVals[i] - primeVals[i - 1]);
         fclose(fp);
         exit(0);
     }
@@ -77,9 +81,11 @@ void parallel()
 {
     int current;
     lastNum = (N + 1) / 2 + 1;
-    for (current = 2; current < lastNum; current++)
+    primeVals = (int *)malloc(lastNum*sizeof(int));
+    // printf("last num: %d thread count: %d\n", lastNum, thread_count);
+    for (current = 2; current < N; current++)
     {
-        printf("%d\n", current);
+        // printf("%d\n", current);
         if (isPrime[current])
         {
             # pragma omp parallel num_threads(thread_count)
@@ -89,7 +95,7 @@ void parallel()
                 for (i = current; i < N; i += current)
                     isPrime[i] = false;
             }
-
+            // printf("For loop is done\n");
             primeVals[primeValSize] = current;
             primeValSize++;
         }
